396baeb6882e9a8d79a0ed2c6e9f65e6b325cf1b
Kevin Redman <kevin.r@myplanetdigital.com>
2013-12-14 03:58:15 -0500

    - STABLE Rhytmic - Red Audio

--- src/files/js/main.js
+++ src/files/js/main.js
@@ -1,131 +1,138 @@
-var bufferLoader,
-    context = window.AudioContext ? window.AudioContext : window.webkitAudioContext;
+// var bufferLoader,
+//     context = window.AudioContext ? window.AudioContext : window.webkitAudioContext;
 
-var audio = {
-    context: context,
-    buffer: {},
-    compatibility: {},
-    files: [
-        "../audio/kick.wav",
-        "../audio/snare.wav",
-        "../audio/hat.wav"
-    ],
-    proceed: true,
-    source_loop: {},
-    source_once: {},
-    startTime: context.currentTime + 0.100,
-    tempo: 120, // BPM (beats per minute)
-    eighthNoteTime: (60 / 120) * 2,
-};
+// var audio = {
+//     context: context,
+//     buffer: {},
+//     compatibility: {},
+//     files: [
+//         "../audio/kick.wav",
+//         "../audio/snare.wav",
+//         "../audio/hat.wav"
+//     ],
+//     proceed: true,
+//     source_loop: {},
+//     source_once: {},
+//     startTime: context.currentTime + 0.100,
+//     tempo: 120, // BPM (beats per minute)
+//     eighthNoteTime: (60 / 120) * 2,
+// };
 
-/*
-function play(buffer, time) {
-    // Create a buffer
-    var source = context.createBufferSource();
+// function initialize(bufferLoader, fileList, callback) {
+//     // Set up context
+//     window.AudioContext = window.AudioContext || window.webkitAudioContext;
+//     context = new AudioContext();
 
-    // Connect and set buffer of source
-    source.buffer = buffer;
-    source.connect(context.destination);
-    source.onended(finished)
-    source.start(time);
-}*/
+//     // Load up files
+//     bufferLoader = new BufferLoader(
+//         context,
+//         audio.files,
+//         callback
+//     );
 
-function initialize(bufferLoader, fileList, callback) {
+//     bufferLoader.load();
+// }
+
+// function pause() {
+//     source.stop();
+//     // Measure how much time passed since the last pause.
+//     startOffset += context.currentTime - startTime;
+// }
+
+
+
+// // Assume context is a web audio context, buffer is a pre-loaded audio buffer.
+// var startOffset = 0;
+// var startTime = 0;
+
+// initialize(audio.buffer, audio.file, audio.callback);
+
+
+// total steps
+// current step
+    // ? is active
+RedAudio = function() {
+    this.sequenceStep = 0;
+    this.STEPS_PER_PATTERN = 16;
+    this.steps = [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0];
+    this.buffer = {};
+    this.bufferReturn = {};
+    this.files = [
+        "../audio/kick.wav"
+    ];
+}
+
+RedAudio.prototype.setup = function() {
     // Set up context
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     context = new AudioContext();
 
     // Load up files
-    bufferLoader = new BufferLoader(
+    this.buffer = new BufferLoader(
         context,
-        audio.files,
-        callback
+        this.files,
+        this.sequencerTimerStart.bind(this)
     );
 
-    bufferLoader.load();
+    this.buffer.load();
 }
 
-initialize(bufferLoader, audio.file, finished);
+RedAudio.prototype.sequencerTimerStart = function(bufferReturnData) {
+    // Calculate intervals given the BPM
+    var interval = 60000 / 120 / 4;
+    this.bufferReturn = bufferReturnData;
 
-// Assume context is a web audio context, buffer is a pre-loaded audio buffer.
-var startOffset = 0;
-var startTime = 0;
-
-function pause() {
-    source.stop();
-    // Measure how much time passed since the last pause.
-    startOffset += context.currentTime - startTime;
+    console.log("Setting the timer to ", interval, " seconds");
+    // console.log(buffers);
+    // Create the timer
+    this.sequencerTimer = setInterval(this.sequencerRoutine.bind(this), interval);
 }
 
-function play(buffer) {
+RedAudio.prototype.sequencerRoutine = function() {
+    console.log(this.bufferReturn);
+    // console.log("*** sequenceStep + 1 (" + (this.sequenceStep + 1) + ") === this.status.numberOfPatterns (" + this.status.numberOfPatterns + ") * " + this.STEPS_PER_PATTERN);
+
+    if (this.sequenceStep == this.steps.length)
+        this.sequenceStep = 0;
+    // Play note
+    // if (this.steps[this.sequenceStep] === 1) {
+    //     console.log('HIT');
+    //     // If the next step is active, turn the playing note off.
+    //     kick.stop();
+    //     // If there really is a note (not a pause), play it.
+    //     kick.start(0);
+    // }
+
+    startTime = context.currentTime;
+    var source = context.createBufferSource();
+
+    // Connect graph
+    source.buffer = this.bufferReturn[0];
+    source.connect(context.destination);
+    // Start playback, but make sure we stay in bound of the buffer.
+
+    if(this.steps[this.sequenceStep] === 1)
+        source.start(this.buffer);
+
+    //increment position
+    this.sequenceStep = this.sequenceStep + 1
+}
+
+RedAudio.prototype.get = function() {
+    return this;
+}
+RedAudio.prototype.kick = function() {
     startTime = context.currentTime;
     var source = context.createBufferSource();
     // Connect graph
-    source.buffer = buffer;
+    source.buffer = this.buffer;
     source.connect(context.destination);
     // Start playback, but make sure we stay in bound of the buffer.
-    source.start(0, startOffset % buffer.duration);
+
+    return source; // source.start(0, startOffset % buffer.duration);
 }
 
-function finished (bufferList) {
-    for (var bar = 0; bar < 2; bar++) {
-        var time = startTime + bar * 8 * audio.eighthNoteTime;
-        // Play the bass (kick) drum on beats 1, 5
-        play(bufferList[0], time);
-        // play(bufferList[0], time + 4 * audio.eighthNoteTime);
+var red = new RedAudio();
 
-        // Play the snare drum on beats 3, 7
-        // play(bufferList[1], time + 2 * audio.eighthNoteTime);
-        // play(bufferList[1], time + 6 * audio.eighthNoteTime);
-
-        // Play the hihat every eighth note.
-        for (var i = 0; i < 8; ++i) {
-            play(bufferList[2], time + i * audio.eighthNoteTime);
-        }
-    }
-
-}
-
-
-
-
-RedAudio = {
-    sequenceStep: -1,
-    STEPS_PER_PATTERN: 16
-}
-
-RedAudio.sequencerTimerStart = function() {
-    // Calculate intervals given the BPM
-    var interval = 60000 / 120 / 4;
-    console.log("Setting the timer to ", interval, " seconds");
-    // Create the timer
-    this.sequencerTimer = setInterval(this.sequencerRoutine.bind(RedAudio), interval);
-}
-
-// total steps
-// current step
-    // ? is active
-
-
-
-
-RedAudio.sequencerRoutine = function() {
-    console.log("*** sequenceStep + 1 (" + (this.sequenceStep + 1) + ") === this.status.numberOfPatterns (" + this.status.numberOfPatterns + ") * " + this.STEPS_PER_PATTERN);
-
-    var nextStep = (this.sequenceStep + 1) % (this.STEPS_PER_PATTERN * this.status.numberOfPatterns);
-
-    // Play note
-    if (this.status.steps[nextStep].active === 1) {
-        // If the next step is active, turn the playing note off.
-        this.audioManager.noteOff();
-        // If there really is a note (not a pause), play it.
-        if (this.status.steps[nextStep].note !== -1) {
-            this.audioManager.noteOn(this.status.steps[nextStep].note - 33, Math.round(this.status.steps[nextStep].velocity * 127));
-        }
-    }
-
-    //increment position
-    this.sequenceStep = nextStep;
-}
-
-console.log(RedAudio)
+red.setup();
+console.log('Get red: ', red.get());
